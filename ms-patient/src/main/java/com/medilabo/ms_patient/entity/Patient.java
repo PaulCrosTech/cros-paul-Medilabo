d9d859a684d9a7f8812fd7380d4291889123a85b
Paul CROS <paul.cros.tech@gmail.com>
2025-06-12 09:33:57 +0200

    [SP1] MS Patient : ajout entity, jeu de données en DB

---
+++ ms-patient/src/main/java/com/medilabo/ms_patient/entity/Patient.java
@@ -0,0 +1,45 @@
+package com.medilabo.ms_patient.entity;
+
+import com.medilabo.ms_patient.utils.Gender;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+/**
+ * Entity of the Patient
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "patient_id")
+    private int patientId;
+
+    @Column(name = "first_name", nullable = false, length = 45)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 45)
+    private String lastName;
+
+    @Column(name = "birth_date", nullable = false)
+    private LocalDate birthDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Gender gender;
+
+    @Column(length = 100)
+    private String address;
+
+    @Column(name = "phone_number", length = 12)
+    private String phoneNumber;
+
+}
---
+++ ms-patient/src/main/java/com/medilabo/ms_patient/utils/Gender.java
@@ -0,0 +1,24 @@
+package com.medilabo.ms_patient.utils;
+
+
+import lombok.Getter;
+
+/**
+ * Gender enumeration
+ */
+@Getter
+public enum Gender {
+    M("Masculin"),
+    F("Féminin");
+
+    private final String description;
+
+    /**
+     * Constructor
+     * @param description Description
+     */
+    Gender(String description) {
+        this.description = description;
+    }
+
+}
