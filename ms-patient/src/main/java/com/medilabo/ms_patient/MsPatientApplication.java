e8eb9e79cd94e41c71c73ec02f3be698e035a9c1
Paul CROS <paul.cros.tech@gmail.com>
2025-06-12 10:51:34 +0200

    [SP1] MS Patient : application.properties log level

--- ms-patient/src/main/java/com/medilabo/ms_patient/MsPatientApplication.java
+++ ms-patient/src/main/java/com/medilabo/ms_patient/MsPatientApplication.java
@@ -1,11 +1,13 @@
 package com.medilabo.ms_patient;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Main class for the Patient Microservice
  */
+@Slf4j
 @SpringBootApplication
 public class MsPatientApplication {
 
@@ -16,6 +18,7 @@ public class MsPatientApplication {
      */
     public static void main(String[] args) {
         SpringApplication.run(MsPatientApplication.class, args);
+        log.info("===> MsPatientApplication started <===");
     }
 
 }
