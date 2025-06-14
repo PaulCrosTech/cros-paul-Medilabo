4cddcd3c552e40b88d2984accc4457a99e2ea268
Paul CROS <paul.cros.tech@gmail.com>
2025-06-14 10:40:46 +0200

    [SP1] MS Patient : ajout  route get /patients/{id}

--- ms-patient/src/main/java/com/medilabo/ms_patient/controller/PatientController.java
+++ ms-patient/src/main/java/com/medilabo/ms_patient/controller/PatientController.java
@@ -4,6 +4,7 @@ import com.medilabo.ms_patient.entity.Patient;
 import com.medilabo.ms_patient.service.IPatientService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +30,7 @@ public class PatientController {
         log.info("====> PatientController initialized <====");
     }
 
-    
+
     /**
      * Retrieves a list of all patients.
      *
@@ -39,5 +40,17 @@ public class PatientController {
     public List<Patient> getPatients() {
         log.info("====> GET /patients : page <====");
         return patientService.findAll();
+    }
+
+    /**
+     * Retrieves a patient by their ID.
+     *
+     * @param id the ID of the patient
+     * @return the patient with the specified ID
+     */
+    @GetMapping(path = "/{id}", headers = "X-API-VERSION=1")
+    public Patient getPatientById(@PathVariable int id) {
+        log.info("====> GET /patients/{} <====", id);
+        return patientService.findById(id);
     }
 }
