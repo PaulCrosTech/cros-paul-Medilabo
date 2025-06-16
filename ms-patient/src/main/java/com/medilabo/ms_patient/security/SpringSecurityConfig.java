0f5ed27b75b3cad92848f3db312a123a5989f7c2
Paul CROS <paul.cros.tech@gmail.com>
2025-06-16 09:04:42 +0200

    [SP1] MS Patient : Spring Security (Basic Auth)

--- ms-patient/src/main/java/com/medilabo/ms_patient/security/SpringSecurityConfig.java
+++ ms-patient/src/main/java/com/medilabo/ms_patient/security/SpringSecurityConfig.java
@@ -1,25 +1,78 @@
 package com.medilabo.ms_patient.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+/**
+ * Spring Security configuration
+ */
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
 
+    @Value("${ms-patient.security.username}")
+    private String username;
+
+    @Value("${ms-patient.security.password}")
+    private String password;
+
+    /**
+     * Configures the security filter chain for the application.
+     *
+     * @param http the HttpSecurity object to configure
+     * @return the configured SecurityFilterChain
+     * @throws Exception if an error occurs during configuration
+     */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().authenticated()
                 )
+                .httpBasic(Customizer.withDefaults())
                 .build();
     }
+
+
+    /**
+     * Configures the UserDetailsService for authentication.
+     * This method creates an in-memory user with the specified username and password.
+     * It uses BCrypt for password encoding.
+     *
+     * @return a UserDetailsService that provides the in-memory user
+     */
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User
+                .withUsername(username)
+                .password(passwordEncoder().encode(password))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    /**
+     * Provides a PasswordEncoder bean for encoding passwords.
+     *
+     * @return a BCryptPasswordEncoder instance
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
