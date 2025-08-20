94ce80cf7fde1abf1857acc027c3836a6f81c131
Paul CROS <paul.cros.tech@gmail.com>
2025-08-20 12:06:05 +0200

    Frontend : correction css pour que l'image soit responsive

--- client-ui/frontend/src/pages/HomeView.tsx
+++ client-ui/frontend/src/pages/HomeView.tsx
@@ -2,7 +2,9 @@ function HomeView() {
 
     return (
         <>
-            <img src="/image/logoFull.jpeg" alt="Logo Medilabo" className="w-full h-auto"/>
+            {/*<img src="/image/logoFull.jpeg" alt="Logo Medilabo" className="w-full h-auto"/>*/}
+            <img src="/image/logoFull.jpeg" alt="Logo Medilabo" className="homepage-img-responsive"/>
+
         </>
     );
 }
